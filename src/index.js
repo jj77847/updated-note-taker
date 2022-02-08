1e58c89d00a006b71fe0f7ee90bd88de47f2f57c
jj77847 <81172135+jj77847@users.noreply.github.com>
2022-02-08 13:14:13 +0000

    index.js confirming test

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const routes = require("./routes");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middle-Ware
 app.use(express.static("public"));
